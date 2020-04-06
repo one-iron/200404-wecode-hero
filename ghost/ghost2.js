7214ef4731ee94b98774a67cca1750b1da79bec7
oneiron <admin@adminui-MacBookPro.local>
2020-04-06 13:51:34 +0900

    modify 0405

--- ghost/ghost2.js
+++ ghost/ghost2.js
@@ -1,33 +1,36 @@
 function start(){
     const bg = document.getElementById("bg");
     const ghostDiv = document.getElementsByClassName("ghostFront");
- 
+    let heroDiv = document.querySelector(".front");
     let ghostKill = 0;
     let ghostCount = 0;
     
-    
-    let ghostStart = setInterval(makingGhost, 200);
+    let ghostStart = setInterval(makingGhost, 1500);
     function makingGhost(){
-        const ghost = document.createElement("Div");
-        let ghostTopNum = 0;
-    
+
         //고스트 클래스를 만들고, x축 랜덤 설정, y축 0 설정하여 bg 아이디에 추가
+        const ghost = document.createElement("Div");
         ghost.className = "ghostFront";
         ghost.style.left = Math.floor(Math.random() * 755) + "px";
         ghost.style.top ="0px";
         document.getElementById("bg").appendChild(ghost);
     
-        //0.1초마다 y축 5px추가하여 내려가게 하기
+        
+        //고스트를 움직이고, 죽이는 것을 반복하는 함수 입니다.
         let ghostPosition = setInterval(movingGhost,100);
+        let ghostTopNum = 0;
+
         function movingGhost(){
-            let leftStyle = document.getElementById("location").style.left;
-            let heroLeft = leftStyle.slice(0,leftStyle.indexOf('px')) * 1;
-            const heart = document.getElementsByClassName("heart")[0];
             const killCount = document.getElementsByClassName("killCount")[0];
             const saveCount = document.getElementsByClassName("ghostCount")[0];
-    
+
+            //hero left 정확한 값을 가져오지 못해서 css style left로 접근해서 px을 없애고, 숫자만 가져왔습니다.
+            let leftStyle = document.getElementById("location").style.left;
+            let heroLeft = leftStyle.slice(0,leftStyle.indexOf('px')) * 1;
+            
+            //0.1초마다 y축 5px추가하여 내려가게 하기
             ghost.style.top = `${ghostTopNum+=5}px`;
-    
+
             //히어로가 고스트 박스에 중첩하면 고스트가 죽은 이미지로 전환, 죽은 이미지가 1초 후 없어지게 하기, 고스트 킬 카운트 +1
             if(ghost.offsetTop > 480 && 
                 ghost.offsetLeft < heroLeft && heroLeft < ghost.offsetLeft + 47 || // 히어로 왼쪽 선 기준으로 고스트 범위 박스 에 들어갈 때
@@ -49,20 +52,6 @@ function start(){
                 bg.removeChild(ghostDiv[0]);
                 clearInterval(ghostPosition);
             }
-    
-            //히어로가 고스트를 죽인 횟수에 따른 하트 게이지 삭제 및 3번 넘으면 게임 종료
-            if(ghostKill === 1){
-                heart.style.backgroundPosition = "-10px 65px";
-            }
-            else if(ghostKill === 2){
-                heart.style.backgroundPosition = "-10px 30px";
-            }
-            else if(ghostKill === 3){
-                heart.style.display = "none";
-                ghostDiv.style.display = "none";
-                killCount.innerHTML = `죽인 고스트 : 3`;
-                clearInterval(ghostStart);
-            }            
         }
     }
 }
--- hero/hero.js
+++ hero/hero.js
@@ -1,30 +1,29 @@
 let heroLocation = document.getElementById("location");
 let heroDiv = document.querySelector(".front");
-let heroX = 400;
+let heroX = 383;
 
-window.addEventListener('keyup', function(e){
-    // 왼쪽 버튼을 누르고 히어로 위치가 끝이면 움직이지 않게, 끝이 아니면 20을 뺀 위치로
-    if(e.keyCode === 37 || e.keyCode === 36){
-        if(heroX === 20 || e.keyCode === 36){
+window.addEventListener('keydown', function(e){
+
+    // 왼쪽 버튼(37)을 누르고 히어로 위치가 끝(40)이면 움직이지 않게, 끝이 아니면 40을 뺀 위치로
+    if(e.keyCode === 37){
+        if(heroX === 3){
             heroDiv.className = "left";
-            heroX = 20;
-            heroLocation.style.left = "20px";
+            heroLocation.style.left = `${heroX}px`;
         }
         else{
             heroDiv.className = "left";
-            heroX = heroX - 20;
+            heroX = heroX - 10;
             heroLocation.style.left = `${heroX}px`;
         }
     }
-    // 오른쪽 버튼을 누르고 히어로 위치가 끝이면 움직이지 않게, 끝이 아니면 20을 더한 위치로
-    else if(e.keyCode === 39 || e.keyCode === 35){
-        if(heroX === 780 || e.keyCode === 35){
+    // 오른쪽 버튼(39)을 누르고 히어로 위치가 끝(760)이면 움직이지 않게, 끝이 아니면 40을 더한 위치로
+    else if(e.keyCode === 39){
+        if(heroX === 763){
             heroDiv.className = "right";
-            heroX = 780;
-            heroLocation.style.left = "780px";   
+            heroLocation.style.left = `${heroX}px`;   
         }
         else {
-            heroX = heroX + 20
+            heroX = heroX + 10
             heroDiv.className = "right";
             heroLocation.style.left = `${heroX}px`;
         }
@@ -33,5 +32,8 @@ window.addEventListener('keyup', function(e){
     // 아래쪽 버튼을 누르면 정면 이미지 클래스로 전환
     else if(e.keyCode === 40){
         heroDiv.className = "front";
+    }
+    else if(e.keyCode === 38){
+        heroDiv.className = "back";
     }
 })
